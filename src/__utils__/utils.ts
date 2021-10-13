dd2c141acabc370bc5e261e73c4e2aeeb3a88044
Jani Anttonen <jantto@utu.fi>
2021-10-13 15:49:27 +0300

    Include the RPC_URL in env in GitHub actions

--- src/__utils__/utils.ts
+++ src/__utils__/utils.ts
@@ -5,9 +5,9 @@ jest.setTimeout(240000)
 
 global.testProvider =
   process.env.RPC_URL &&
-  process.env.RPC_URL !== '' &&
+  process.env.RPC_URL.length > 0 &&
   process.env.RPC_URL[0] === 'w'
     ? new providers.WebSocketProvider(process.env.RPC_URL, network)
-    : process.env.RPC_URL[0] === 'h'
+    : process.env.RPC_URL.length > 0 && process.env.RPC_URL[0] === 'h'
     ? new providers.JsonRpcProvider(process.env.RPC_URL, network)
     : providers.getDefaultProvider()
