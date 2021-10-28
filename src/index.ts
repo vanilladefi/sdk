21918f6181057657584c38d8f7b6045c0a675f2e
Jani Anttonen <jantto@utu.fi>
2021-10-28 15:17:17 +0300

    Avoid default export

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,6 @@
-import * as contracts from './contracts'
-import * as ipfs from './ipfs'
-import * as migration from './migration'
-import * as tokens from './tokens'
-import * as trades from './trades'
-import * as users from './users'
-
-export default {
-  ...contracts,
-  ...ipfs,
-  ...migration,
-  ...tokens,
-  ...trades,
-  ...users,
-}
+export * from './contracts'
+export * from './ipfs'
+export * from './migration'
+export * from './tokens'
+export * from './trades'
+export * from './users'
