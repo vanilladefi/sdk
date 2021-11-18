a3361741165ec37391f905d334426cfbe5bdae72
Jani Anttonen <jantto@utu.fi>
2021-11-18 16:21:22 +0200

    Fix test runners

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,7 @@
 module.exports = {
   preset: 'ts-jest',
   moduleDirectories: ['node_modules', '<rootDir>', 'src'],
+  testMatch: [ "**/__tests__/**/*.[jt]s?(x)" ],
   setupFiles: ["<rootDir>/src/__utils__/env.ts"],
   setupFilesAfterEnv: ["<rootDir>/src/__utils__/utils.ts"]
 }
