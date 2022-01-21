323f993dbee5b8d0cc57e6318c8ac40b515ac390
muzam1l <muzamilsofi@outlook.com>
2022-01-21 20:55:39 +0530

    Implemented an unsafe util!

--- packages/stake/src/contracts.ts
+++ packages/stake/src/contracts.ts
@@ -2,17 +2,18 @@ import { getNetwork } from '@ethersproject/networks'
 import { providers, Signer } from 'ethers'
 import { IJuiceStaking__factory } from './types/juicenet/factories/IJuiceStaking__factory'
 import { IJuiceStaking } from './types/juicenet/IJuiceStaking'
+import { preferences } from './preferences'
 
 export const getJuiceStakingContract = (
   signerOrProvider?: Signer | providers.Provider,
 ): IJuiceStaking => {
   return IJuiceStaking__factory.connect(
-    contractAddress,
+    getContractAddress(),
     signerOrProvider || providers.getDefaultProvider(networks.mainnet),
   )
 }
 
-export const contractAddress = ''
+export const getContractAddress = () => preferences.contractAddress
 
 export const networks = {
   mainnet: getNetwork('matic'),
