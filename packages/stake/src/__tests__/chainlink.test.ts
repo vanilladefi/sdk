8268b0319ae278e2912b005abd9a15a0e59f505b
JaniAnttonen <jani.anttonen@protonmail.ch>
2022-02-03 12:08:39 +0200

    Update chainlink tests

--- packages/stake/src/__tests__/chainlink.test.ts
+++ packages/stake/src/__tests__/chainlink.test.ts
@@ -1,6 +1,6 @@
 import { getTokenPrice } from '../chainlink'
 import v2Tokens from '../tokenLists/tokens_v2_0.json'
-import { testProvider } from '../__utils__/utils'
+import { testPolygonProvider } from '../__utils__/utils'
 
 test('Price fetching from Chainlink', async () => {
   try {
@@ -9,8 +9,8 @@ test('Price fetching from Chainlink', async () => {
       decimals: number
       address: string
     }) => {
-      const price = await getTokenPrice(token.address, testProvider)
-      expect(price).toBeGreaterThan(0)
+      const price = await getTokenPrice(token.address, testPolygonProvider)
+      expect(price.answer.toNumber()).toBeGreaterThan(0)
     }
     await Promise.all(v2Tokens.map(priceGetter))
   } catch (_e) {
