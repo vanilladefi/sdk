be76301b3ba51720d8538a115bcf997a0635f316
JaniAnttonen <jani.anttonen@protonmail.ch>
2022-02-11 13:16:24 +0200

    Use both testproviders in tests

--- packages/stake/src/__tests__/contracts.test.ts
+++ packages/stake/src/__tests__/contracts.test.ts
@@ -1,11 +1,14 @@
 import { getBasicWalletDetails } from '../contracts'
-import { testPolygonProvider } from '../__utils__/utils'
+import { testEthereumProvider, testPolygonProvider } from '../__utils__/utils'
 const hardhatTestAccount = '0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266'
 
 test('Get JUICE, MATIC, ETH and VNL balances', async () => {
   const { maticBalance, juiceBalance } = await getBasicWalletDetails(
     hardhatTestAccount,
-    { provider: testPolygonProvider },
+    {
+      polygonProvider: testPolygonProvider,
+      ethereumProvider: testEthereumProvider,
+    },
   )
   expect(Number(maticBalance)).toBeGreaterThan(0)
   expect(Number(juiceBalance)).toBeGreaterThan(0)
