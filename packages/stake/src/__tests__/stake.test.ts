3f4b42b38bc513df0653d47efd0f52538a7cf4e4
JaniAnttonen <jani.anttonen@protonmail.ch>
2022-03-08 01:18:05 +0200

    Add leaderboard tests

--- packages/stake/src/__tests__/stake.test.ts
+++ packages/stake/src/__tests__/stake.test.ts
@@ -1,6 +1,6 @@
 import { epoch } from '@vanilladefi/core-sdk'
 import { formatUnits } from 'ethers/lib/utils'
-import { getUserJuiceDelta } from '../stake'
+import { getLeaderboard, getUserJuiceDelta } from '../stake'
 import { testPolygonProvider } from '../__utils__/utils'
 const hardhatTestAccount = '0x35434a35C19cabC4bD508Edf538cC1Cf33BDA688'
 
@@ -10,4 +10,30 @@ test('Get user JUICE delta', async () => {
     optionalAddress: process.env.JUICE_ADDRESS,
   })
   expect(formatUnits(delta, 8)).not.toBe(0)
+})
+
+test('Get leaderboard', async () => {
+  const limit = 5
+  const defaultLimit = 10
+
+  // Test that limiting the leaderboard to 5 users works
+  const leaderboard = await getLeaderboard(epoch, 'latest', limit, {
+    polygonProvider: testPolygonProvider,
+    optionalAddress: process.env.JUICE_ADDRESS,
+  })
+  expect(leaderboard.length).toBe(limit)
+
+  // Test that the by default the leaderboard's size is 10
+  const leaderboard2 = await getLeaderboard(epoch, 'latest', undefined, {
+    polygonProvider: testPolygonProvider,
+    optionalAddress: process.env.JUICE_ADDRESS,
+  })
+  expect(leaderboard2.length).toBe(defaultLimit)
+
+  // Test that leaderboard is in descending order
+  let previous = leaderboard2[0]
+  leaderboard2.slice(1).forEach((user) => {
+    expect(previous[1].gte(user[1])).toBeTruthy()
+    previous = user
+  })
 })
