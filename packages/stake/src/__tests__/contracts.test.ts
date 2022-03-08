f3d11cebb8ed2feae3bb7c73c7d2e0263436691e
JaniAnttonen <jani.anttonen@protonmail.ch>
2022-03-08 16:24:53 +0200

    Test juice balance with the contract address

--- packages/stake/src/__tests__/contracts.test.ts
+++ packages/stake/src/__tests__/contracts.test.ts
@@ -1,18 +1,14 @@
-import {getBasicWalletDetails} from '../contracts'
-import {testEthereumProvider, testPolygonProvider} from '../__utils__/utils'
-const hardhatTestAccount = '0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266'
+import { getBasicWalletDetails } from '../contracts'
+import { testEthereumProvider, testPolygonProvider } from '../__utils__/utils'
 
 test('Get JUICE, MATIC, ETH and VNL balances', async () => {
-  const {maticBalance, juiceBalance} = await getBasicWalletDetails(
-    hardhatTestAccount,
+  const { juiceBalance } = await getBasicWalletDetails(
+    process.env.JUICE_ADDRESS || '',
     {
       polygonProvider: testPolygonProvider,
       ethereumProvider: testEthereumProvider,
-      optionalAddress: process.env.JUICE_ADDRESS
+      optionalAddress: process.env.JUICE_ADDRESS,
     },
   )
-  expect(Number(maticBalance)).toBeGreaterThan(0)
   expect(Number(juiceBalance)).toBeGreaterThan(0)
-}
-);
-
+})
