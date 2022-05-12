1995875f068b5256592b22efcff9651372255a9f
muzam1l <muzamilsofi@outlook.com>
2022-05-12 22:38:52 +0530

    simple leaderboard logic

--- packages/stake/src/stake.ts
+++ packages/stake/src/stake.ts
@@ -61,10 +61,6 @@ export const getUserJuiceDelta = async (
   const blockThreshold = options?.blockThreshold || 1000
 
   const contract = getJuiceStakingContract(options)
-  const deltaByToken: Record<string, BigNumber> = {}
-  const latestUnstakeByToken: Record<string, number> = {}
-  const firstUnstakeByToken: Record<string, number> = {}
-  let delta = BigNumber.from(0)
 
   const stakeFilter: ethers.EventFilter =
     contract.filters.StakeAdded(userAddress)
@@ -98,66 +94,28 @@ export const getUserJuiceDelta = async (
     )
   ).flat()
 
-  // First, filter every realized JUICE profit
-  unStakes.forEach((event) => {
-    if (event?.args && event?.blockNumber) {
-      const { args, blockNumber } = event
-      const { unstakedDiff, token } = args
-
-      if (deltaByToken[token]) {
-        deltaByToken[token] = deltaByToken[token].add(unstakedDiff)
-      } else {
-        deltaByToken[token] = unstakedDiff
-      }
-
-      if (
-        !firstUnstakeByToken[token] ||
-        firstUnstakeByToken[token] > blockNumber
-      ) {
-        firstUnstakeByToken[token] = blockNumber
-      }
-
-      if (
-        !latestUnstakeByToken[token] ||
-        latestUnstakeByToken[token] < blockNumber
-      ) {
-        latestUnstakeByToken[token] = blockNumber
-      }
-    }
-  })
-
-  if (Object.keys(deltaByToken).length > 0) {
-    const stakes: ethers.Event[] | TypedEvent<any[]>[] = (
-      await Promise.all(
-        ranges.map((range) =>
-          contract.queryFilter(stakeFilter, range.startBlock, range.endBlock),
-        ),
-      )
-    ).flat()
-
-    // Remove stakes from delta
-    stakes.forEach((event) => {
-      if (event?.args && event?.blockNumber) {
-        const { args, blockNumber } = event
-        const { unstakedDiff, token } = args
-        if (
-          Object.keys(deltaByToken).includes(token) &&
-          latestUnstakeByToken[token] > blockNumber
-        ) {
-          // Remove the stakes previous to first unstake from the delta if trimPreviousStake === true
-          if (
-            !(trimPreviousStake && firstUnstakeByToken[token] < blockNumber)
-          ) {
-            deltaByToken[token] = deltaByToken[token].add(unstakedDiff)
-          }
-        }
-      }
-    })
-
-    delta = Object.values(deltaByToken).reduce((_previous, _current) =>
-      _previous.add(_current),
+  const stakes: ethers.Event[] | TypedEvent<any[]>[] = (
+    await Promise.all(
+      ranges.map((range) =>
+        contract.queryFilter(stakeFilter, range.startBlock, range.endBlock),
+      ),
     )
-  }
+  ).flat()
+
+  let delta = BigNumber.from(0)
+
+  // Assuming ustake(U)-stake(S) event pair in case of adding stake to be in same block, while single unstake(U) represents removing stake
+  // [U-S,U-S,U,U,U,U-S,U]
+  stakes.forEach(({ args }) => {
+    if (!args) return
+    const { unstakedDiff }: { unstakedDiff: BigNumber } = args as any
+    delta = delta.add(unstakedDiff.abs())
+  })
+  unStakes.forEach(({ args }) => {
+    if (!args) return
+    const { unstakedDiff }: { unstakedDiff: BigNumber } = args as any
+    delta = delta.sub(unstakedDiff.abs())
+  })
 
   return delta
 }
